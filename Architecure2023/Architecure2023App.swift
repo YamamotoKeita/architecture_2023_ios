78a3850e68ae05ce4643357abce339e7b95570f0
山本敬太 <yamamoto@altonotes.co.jp>
2023-04-28 17:20:14 +0900

    First commit

---
+++ Architecure2023/Architecure2023App.swift
@@ -0,0 +1,17 @@
+//
+//  Architecure2023App.swift
+//  Architecure2023
+//
+//  Created by 山本敬太 on 2023/04/27.
+//
+
+import SwiftUI
+
+@main
+struct Architecure2023App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
